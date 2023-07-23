8449eabf50ea756b9b0f1263b54ffaf0658b86b4
Carolis <redrumjedi@gmail.com>
2023-07-23 14:29:01 -0300

    update carolis info

--- carolis.js
+++ carolis.js
@@ -1,10 +1,15 @@
 #!/usr/bin/env node
-
 "use strict";
+const chalk = require('chalk')
+const figlet = require('figlet');
+
 const { readFile } = require("fs");
 const { join } = require("path");
 
 readFile(join(__dirname, "carolisInfo.json"), "utf8", (err, content) => {
   if (err) throw err;
+  console.log(chalk.magenta(
+    figlet.textSync('Carolis', { font:"slant", horizontalLayout: 'full' })
+  ))
   console.log(content);
 });
