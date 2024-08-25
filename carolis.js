c4b2f6c359820ae86124d242b6a5bc0e10cb65eb
Carolis <redrumjedi@gmail.com>
2024-08-24 22:34:05 -0300

    fix: font case causing enoent and update infos

--- carolis.js
+++ carolis.js
@@ -6,10 +6,12 @@ const figlet = require('figlet');
 const { readFile } = require("fs");
 const { join } = require("path");
 
+const message = chalk.magenta(
+  figlet.textSync('Carolis', { font:"Slant", horizontalLayout: 'full' })
+)
+
 readFile(join(__dirname, "carolisInfo.json"), "utf8", (err, content) => {
   if (err) throw err;
-  console.log(chalk.magenta(
-    figlet.textSync('Carolis', { font:"slant", horizontalLayout: 'full' })
-  ))
+  console.log(message)
   console.log(content);
 });
